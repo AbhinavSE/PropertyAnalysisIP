097a2f7463bd621bb69b2b30f900e29951889b5d
Shaunak Pal <palshaunak7@gmail.com>
2022-02-21 17:20:54 +0400

    Update

--- Code/Scrapers/magicbricks.py
+++ Code/Scrapers/magicbricks.py
@@ -1,7 +1,25 @@
 from .scraper import Scraper
-
+from selenium.webdriver.common.keys import Keys
 
 class MagicbricksScraper(Scraper):
     def __init__(self):
         super().__init__()
         self.index_url = "https://www.magicbricks.com/property-for-sale-rent-in-New%20Delhi/residential-real-estate-New%20Delhi"
+    
+    def search(self):
+        search_location_div = self.driver.find_element_by_id("keyword_autoSuggestSelectedDiv")
+
+        chosen_location_elements = search_location_div.find_element_by_class_name("mb-search__tag")
+        for element in chosen_location_elements:
+            close_button = chosen_location_elements.find_element_by_class_name("mb-search__tag-close")
+            close_button.click()
+        search_location_box = search_location_div.find_element_by_id("keyword")
+
+        search_location_box.send_keys(self.location)
+        search_location_box.send_keys(Keys.RETURN)
+
+    def run(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+        self.search()
+        
--- Code/Scrapers/scraper.py
+++ Code/Scrapers/scraper.py
@@ -1,23 +1,12 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class Scraper:
     def __init__(self):
-        MagicBricksURL = "https://www.magicbricks.com/property-for-sale-rent-in-New%20Delhi/residential-real-estate-New%20Delhi"
-        Location = "New Delhi"
+        self.location = "New Delhi"
+        self.driver = webdriver.Chrome(executable_path=os.getcwd() + "/chromedriver")
 
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.get(MagicBricksURL)
-
-        search_location_div = driver.find_element_by_id("keyword_autoSuggestSelectedDiv")
-
-        chosen_location_elements = search_location_div.find_element_by_class_name("mb-search__tag")
-        for element in chosen_location_elements:
-            close_button = chosen_location_elements.find_element_by_class_name("mb-search__tag-close")
-            close_button.click()
-        search_location_box = search_location_div.find_element_by_id("keyword")
-
-        search_location_box.send_keys(Location)
-        search_location_box.send_keys(Keys.RETURN)
+    def search(self):
+        pass
