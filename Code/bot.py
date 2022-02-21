587afdeafadb2167b9afdac90e6d48cc40e0be20
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-02-21 16:52:50 +0400

    Initializing repo

---
+++ Code/bot.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+MagicBricksURL = "https://www.magicbricks.com/property-for-sale-rent-in-New%20Delhi/residential-real-estate-New%20Delhi"
+Location = "New Delhi"
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(MagicBricksURL)
+
+search_location_div = driver.find_element_by_id("keyword_autoSuggestSelectedDiv")
+
+chosen_location_elements = search_location_div.find_element_by_class_name("mb-search__tag")
+for element in chosen_location_elements:
+    close_button = chosen_location_elements.find_element_by_class_name("mb-search__tag-close")
+    close_button.click()
+search_location_box = search_location_div.find_element_by_id("keyword")
+
+search_location_box.send_keys(Location)
+search_location_box.send_keys(Keys.RETURN)
