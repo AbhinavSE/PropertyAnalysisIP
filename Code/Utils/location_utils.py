d29704dd29c7d97c1703360b2bf55180a69c51f5
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-03-06 19:28:56 +0400

    Created utils folder and location_utils.py

--- Code/Utils/location_utils.py
+++ Code/Utils/location_utils.py
@@ -1,4 +1,18 @@
 import pandas as pd
 import geopandas as gpd
 import geopy
-from geopy.geocoders import Nominatim
+import geocoder
+
+#Parameters
+PROJECT_NAME = "PropertyDataAnalysis"
+API_KEY = "AhbvYTADoexgk9EW4R5SyOu92avR1sTw"
+
+def get_location(address):
+    g = geocoder.tomtom(address, key=API_KEY)
+    return g.latlng
+
+def get_location_series(address_series):
+    location_series = pd.Series()
+    for address in address_series:
+        location_series.append(get_location(address))
+    return location_series
